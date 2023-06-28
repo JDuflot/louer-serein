9a3e597d9dfb24481fab513ca8a44e4498666d4b
JDuflot <124251449+JDuflot@users.noreply.github.com>
2023-06-28 22:01:34 +0200

    feat:templates Chat view

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Chat; 
+use Doctrine\ORM\EntityManagerInterface; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -9,12 +11,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChatController extends AbstractController
 {
+    private $entityManager; // Déclarez une propriété privée pour stocker l'instance de l'EntityManager
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager; // Injectez l'EntityManager via le constructeur
+    }
+
     #[Route('/chat', name: 'app_chat')]
     #[IsGranted("ROLE_USER")]
     public function index(): Response
     {
+        $chatRepository = $this->entityManager->getRepository(Chat::class);
+        $chat = $chatRepository->findAll();
+
+        $user = $this->getUser();
+
         return $this->render('chat/index.html.twig', [
             'controller_name' => 'ChatController',
+            'user' => $user,
+            'chat' => $chat,
         ]);
     }
 }
